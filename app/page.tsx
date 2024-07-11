264735c00f3770fcd4316771a60af24f03b1919e
jhonsnow456 <thakuraman22july@gmail.com>
2024-07-12 03:22:54 +0530

    fix: :ambulance: Hotfix for Approch section

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
-        {/* <Approach /> */}
+        <Approach />
         <Footer />
       </div>
     </main>
